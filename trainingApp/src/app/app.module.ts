5a981c7ae599b72f43fc5aeed2d872291cb9046d
Tanuja <tanuja.s@robosoftin.com>
2022-11-24 08:58:00 +0530

    "header,footer,training_count,navbar"

--- trainingApp/src/app/app.module.ts
+++ trainingApp/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';  
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TrainingCountComponent } from './training-count/training-count.component';
+import { NavbarComponent } from './navbar/navbar.component';  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TrainingCountComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ trainingApp/src/app/training-count/training-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-count',
+  templateUrl: './training-count.component.html',
+  styleUrls: ['./training-count.component.css']
+})
+export class TrainingCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
